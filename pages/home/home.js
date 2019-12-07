3bd7c7632af47ed999d31f86efcc897b7de4ba6c
Tony0822 <constyang@163.com>
2019-12-07 21:50:28 +0800

    小程序初体验

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,62 +5,29 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    name:"师姐",
+    age:18,
+    students:[
+      { id: 1, name: "A", age: 18 },
+      { id: 2, name: "B", age: 19 },
+      { id: 3, name: "C", age: 10 },
+      { id: 4, name: "D", age: 21 }
+    ],
+    counter: 0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  plusNumClick() {
+    // 1.错误的做法：界面是不会刷新的
+    // this.data.counter += 1
+    // console.log(this.data.counter)
+    this.setData({
+      // console.log(this.data.counter),
+      counter: this.data.counter + 1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  subNumClick(){
+    this.setData({
+      counter: this.data.counter - 1
+    })
   }
+  
 })
